267296162e3243f947ddd537a9b9b6180fcf4c65
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-26 23:02:12 -0400

    First commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+async function run() {
+  const out = await exec.getExecOutput('zeet job:run', [
+    core.getInput('project') || core.getInput('project_id'),
+    `--build=${core.getInput('build')}`,
+    `--wait=${core.getBooleanInput('wait')}`
+  ])
+
+  const links = out.stdout.match('(https?:\\/\\/zeet\\.co\\/repo[^\\s]+)')
+  core.setOutput('link', links ? links[0] : 'Not Found')
+}
+
+run()
